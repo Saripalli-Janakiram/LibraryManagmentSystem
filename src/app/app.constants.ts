f2d83736b7a17e8c643656d0005fa1e6293806d8
JANAKIRAM SARIPALLI <janakiram.saripalli@outlook.com>
2023-09-19 23:59:42 -0500

    Angular Login Form Creation anf Material Module created and imported in app module.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,6 @@
 export class appConstants{
-public static applicationName:string ='Library Management';
+public static applicationName : string ='Library Management';
+public static logIn : string ='Login';
+public static userName : string = 'UserName';
+public static password : string = 'Password';
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { appConstants } from '../app.constants';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  login  = appConstants.logIn;
+  password = appConstants.password;
+  userName = appConstants.userName;
 }
