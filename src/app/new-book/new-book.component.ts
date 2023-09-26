d643c6fcb4bb81fbb32af74c31d2870534f02a77
JANAKIRAM SARIPALLI <janakiram.saripalli@outlook.com>
2023-09-26 13:11:39 -0500

    UI design for adding new books to the database.

--- src/app/new-book/new-book.component.ts
+++ src/app/new-book/new-book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl, RequiredValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-book',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-book.component.scss']
 })
 export class NewBookComponent {
+ constructor(){}
+ addBookForm = new FormGroup({
+  ctrlBookName: new FormControl("",Validators.required),
+  ctrlAuthorName: new FormControl("",Validators.required),
+  ctrlVolume: new FormControl("",Validators.required),
+  ctrlNoOfBooks: new FormControl("",Validators.required)
+ })
 
+ public reset(){
+  this.addBookForm.reset();
 }
+public testData(){
+  if(this.addBookForm.valid){
+    console.log(this.addBookForm.controls.ctrlBookName.value);
+    console.log(this.addBookForm.controls.ctrlAuthorName.value);
+    console.log(this.addBookForm.controls.ctrlVolume.value);
+    console.log(this.addBookForm.controls.ctrlNoOfBooks.value);
+    }
+    this.reset();
+}
+}
+
